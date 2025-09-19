f73aa06403bc3aacd2765306a20c4f3e45754441
Kuro <Kuro@Kuro.lan>
2025-09-19 21:26:59 +0700

    nộp lab 1

--- Lab1/src/main/java/com/mycompany/lab1/bai1.java
+++ Lab1/src/main/java/com/mycompany/lab1/bai1.java
@@ -1,20 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+//Name : Phạm Minh Hoàng 
+//Mssv : PS47440
 
 package com.mycompany.lab1;
 
-/**
- *
- * @author ICT
- */
+import java.util.Scanner;
+
 public class bai1 {
 
-    public static void main(String[] args) {
-        //tính trung bình 3 số nguyên 
-        double a = 6, b = 7, c = 5;
-        double Tinhtrungbinh;
-        Tinhtrungbinh = (a * 2 + b + c) / 4;
-        System.out.println("TRUNG BNH: " + Tinhtrungbinh);
+   public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+        System.out.println("hoten : ");
+        String hoten = sc.nextLine();
+        System.out.print("diemTB: "); 
+        double diemTB = sc.nextDouble();
+        System.out.printf("%s, %.2f diem\n", hoten , diemTB);
     }
+
 }
